1e0ead36a295913e0a05e14c1aabffcaac446e0a
uniquemozilla22 <uniqgaming26@gmail.com>
2022-10-04 01:14:42 +0545

    Portfolio Web Added

---
+++ README.md
@@ -0,0 +1,33 @@
+<div align="center">
+<h2> 𝐇𝐞𝐥𝐥𝐨 𝐭𝐡𝐞𝐫𝐞, 𝐟𝐞𝐥𝐥𝐨𝐰 <𝚍𝚎𝚟𝚎𝚕𝚘𝚙𝚎𝚛𝚜/>! <img src="https://github.com/ABSphreak/ABSphreak/blob/master/gifs/Hi.gif" width="30px"></h2>
+</div>
+
+<div align="center" width="50">
+
+<img src="https://user-images.githubusercontent.com/49606627/115220420-3f6ffe00-a128-11eb-89cb-219b2a33de17.png" alt="Welcome!" width="1000"/>
+
+</div>
+
+<div align="center">
+
+You have finally discovered my Github profile. <br>
+Please feel free to clone/fork projects, raise issues and submit PRs if you think something could be better. <br>
+Ask me anything <a href="mailto:uniq.funkii@gmail.com"><b>here</b></a><br>
+or <a href="mailto:uniq.funkii@gmail.com"><b>email</b></a> me
+
+<i>Happy Coding!</i> 😊
+
+</div>
+
+<div align="center">
+  <img src="https://github-readme-stats.vercel.app/api?username=uniquemozilla22&count_private=true" alt="Yogesh bhattarai's stats"/>
+
+</br>
+<div align="center">
+  <p> Follow me on Behance and see some of my designs <a href="https://www.behance.net/yogeshbhattarai1">Behance Profile Yogesh Bhattarai</a> </p>
+  <img src="https://user-images.githubusercontent.com/49606627/174067776-e9d4f030-31b9-4980-afe6-c1ae98334ddc.jpg" alt="Yogesh Bhattarai at Behance"/>
+</div>
+</br>
+<i>Random dev joke for you! (create your own by clicking here ↓)</i><br>
+<a href="https://readme-jokes.vercel.app"><img align="center" src="https://readme-jokes.vercel.app/api" alt="README Jokes"></a>
+</div>
---
+++ src/content_option.js
@@ -0,0 +1,192 @@
+import image_me from "./assets/images/me.jpeg";
+
+const logotext = "Yogesh B.";
+const meta = {
+  title: "Yogesh Bhattarai",
+  description:
+    "I’m Yogesh Web Developer _ Full stack devloper,currently working in Takeo.ai",
+};
+
+const introdata = {
+  title: "I’m Yogesh Bhattarai",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I make beautiful UI designs",
+  },
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+  your_img_url: image_me,
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Full Stack React Developer",
+    where: "Takeo.ai, New York",
+    date: "Current",
+  },
+  {
+    jobtitle: "React Developer",
+    where: "Amazing IT Centre",
+    date: "8 months",
+  },
+  {
+    jobtitle: "Frontend Web Developer",
+    where: "Digital Nepal",
+    date: "6 months",
+  },
+  {
+    jobtitle: "Full Stack Developer",
+    where: "Kaaikaas Innovations Pvt Ltd",
+    date: "1 year 7 mths",
+  },
+];
+
+const skills = [
+  {
+    name: "Node Js",
+    value: 85,
+  },
+  {
+    name: "Javascript",
+    value: 99,
+  },
+  {
+    name: "React",
+    value: 99,
+  },
+  {
+    name: "Jquery",
+    value: 85,
+  },
+  {
+    name: "UI Design",
+    value: 80,
+  },
+];
+
+const services = [
+  {
+    title: "UI & UX Design",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Web Apps",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Full stack Application",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "bhattaraiyogesh007@gmail.com",
+  YOUR_FONE: "(977)9846779494",
+  description: "Lets create something beautiful together",
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/uniquemozilla22",
+  linkedin: "https://linkedin.com/in/yogesh-bhattarai-6b1112178/",
+  twitter: "https://twitter.com/yogesh_creator",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
