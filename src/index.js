bc59927900cecf3aae699d443166d374142bca6e
Yogesh Bhattarai <49606627+uniquemozilla22@users.noreply.github.com>
2025-04-21 13:49:29 -0400

    Google Analytics

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./app/App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
+
 env.config();
 
 ReactDOM.render(<App />, document.getElementById("root"));
@@ -13,3 +14,4 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
